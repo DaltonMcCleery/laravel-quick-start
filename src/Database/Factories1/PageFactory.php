f575c83fbcf645c5b9e2b69836f17e471bc5773d
DaltonMcCleery <daltonmccleery@gmail.com>
2021-05-03 10:34:06 -0500

    folder renaming

---
+++ src/Database/Factories1/PageFactory.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace DaltonMcCleery\LaravelQuickStart\Database\Factories1;
+
+use DaltonMcCleery\LaravelQuickStart\Models\Page;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PageFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Page::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            'name' => $this->faker->uuid,
+            'title' => $this->faker->uuid,
+            'slug' => $this->faker->slug,
+            'template' => 'page',
+            'active' => true,
+            'meta_title' => $this->faker->realText(50),
+            'og_title' => $this->faker->realText(50),
+            'meta_description' => $this->faker->realText(200),
+            'og_description' => $this->faker->realText(200),
+            'meta_keywords' => 'test, metadata, factory',
+            'social_image' => $this->faker->imageUrl()
+        ];
+    }
+}
