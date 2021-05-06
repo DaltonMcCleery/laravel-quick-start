f8e0c5288b5a06633807be156f8498810e872db9
DaltonMcCleery <daltonmccleery@gmail.com>
2021-05-06 14:42:54 -0500

    Updates: ContentTrait to include Reusable Blocks

--- src/Traits/ContentTrait.php
+++ src/Traits/ContentTrait.php
@@ -19,45 +19,77 @@ use ElevateDigital\CharcountedFields\TextareaCounted;
  */
 trait ContentTrait
 {
-    /**
-     * The content that belongs to the Flexible Components.
-     *
-     * @param bool $showHelp
-     */
-    protected function flexibleComponents($showHelp = true)
+	/**
+	 * The content that belongs to the Flexible Components.
+	 *
+	 * @param bool $showHelp
+	 * @param bool $showReusable
+	 */
+    protected function flexibleComponents($showHelp = true, $showReusable = true)
     {
-        return Flexible::make('Content', 'content')
+        $content = Flexible::make('Content', 'content')
             ->help($showHelp ? 'Page Content can only be edited/added/deleted directly from the Page Resource' : '')
             ->button('Add Content')
             ->fullWidth()
             ->collapsed(true)
 
             // HTML
-            ->addLayout('Textbox (WYSIWYG)', 'wysiwyg', [
-                Select::make('Width', 'width')
-                    ->options([
-                        'slim' => 'Slim (70%)',
-                        'full' => 'Full (100%)'
-                    ])->displayUsingLabels()
-                    ->stacked()->rules('required'),
-
-                $this->wysiwyg()
-            ])
+            ->addLayout('Textbox (WYSIWYG)', 'wysiwyg', $this->wysiwyg(true))
 
             // Add More Layouts Here...
         ;
+
+        if ($showReusable) {
+	        // Reusable Blocks
+	        $content->addLayout('Reusable Block', 'reusable_block', $this->reusable_block());
+        }
+
+        return $content;
     }
 
-    // ----------------------------------------------- HELPER METHODS ----------------------------------------------- //
+    // ----------------------------------------------- LAYOUT METHODS ----------------------------------------------- //
 
-    public function wysiwyg() {
-        return
-            Tiptap::make('Text', 'text')
-                ->buttons([
-                    'heading', 'italic', 'bold', 'code', 'link', 'strike', 'underline', 'superscript', 'subscript',
-                    'bullet_list', 'ordered_list', 'code_block', 'blockquote', 'table', 'edit_html'
-                ])
-                ->headingLevels([2, 3, 4])
-                ->stacked()->rules('required');
+    public function wysiwyg($showWidth = false) {
+    	if ($showWidth) {
+		    return [
+			    Select::make('Width', 'width')
+				    ->options([
+					    'slim' => 'Slim (70%)',
+					    'full' => 'Full (100%)'
+				    ])->displayUsingLabels()
+				    ->stacked()->rules('required'),
+
+			    Tiptap::make('Text', 'text')
+				    ->buttons([
+					    'heading', 'italic', 'bold', 'code', 'link', 'strike', 'underline', 'superscript', 'subscript',
+					    'bullet_list', 'ordered_list', 'code_block', 'blockquote', 'table', 'edit_html'
+				    ])
+				    ->headingLevels([2, 3, 4])
+				    ->stacked()->rules('required')
+		    ];
+	    }
+
+    	return
+		    Tiptap::make('Text', 'text')
+			    ->buttons([
+				    'heading', 'italic', 'bold', 'code', 'link', 'strike', 'underline', 'superscript', 'subscript',
+				    'bullet_list', 'ordered_list', 'code_block', 'blockquote', 'table', 'edit_html'
+			    ])
+			    ->headingLevels([2, 3, 4])
+			    ->stacked()->rules('required');
+    }
+
+    public function reusable_block() {
+    	return [
+		    Select::make('Block', 'block')
+			    ->sortable()->stacked()->required()
+			    ->options(
+				    \DaltonMcCleery\LaravelQuickStart\Models\ReusableBlock::where('active', 1)->get()
+					    ->mapWithKeys(function ($block) {
+						    return [$block->id => ['label' => $block->name]];
+					    })->toArray()
+			    )
+			    ->displayUsingLabels(),
+	    ];
     }
 }
