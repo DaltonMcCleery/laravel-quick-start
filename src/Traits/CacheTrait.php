0f4d020c182af8edcf59bec7b55c1889c93e0b0b
DaltonMcCleery <daltonmccleery@gmail.com>
2021-06-17 13:43:07 -0500

    Updates: CacheTrait capabilities (adding tag/flush all methods)

--- src/Traits/CacheTrait.php
+++ src/Traits/CacheTrait.php
@@ -3,6 +3,7 @@
 namespace DaltonMcCleery\LaravelQuickStart\Traits;
 
 use Illuminate\Support\Facades\Cache;
+use Illuminate\Filesystem\Filesystem;
 
 /**
  * Trait CacheTrait
@@ -13,12 +14,58 @@ trait CacheTrait
 	/**
 	 * Set a new keyed Cache value with an optional timeout period
 	 *
-	 * @param $key
+	 * @param string $key
 	 * @param $data
-	 * @param $hours
+	 * @param int $hours
+	 * @param string|array|null $tags
 	 */
-	public function setCache($key, $data, $hours = 4) {
-		Cache::put($key, $data, now()->addHours($hours));
+	public function setCache(string $key, $data, int $hours = 4, string|array $tags = null) {
+		if ($tags) {
+			Cache::tags($tags)->put($key, $data, now()->addHours($hours));
+		} else {
+			Cache::put($key, $data, now()->addHours($hours));
+		}
+	}
+
+	/**
+	 * Get a keyed Cache value
+	 * If no value found, an optional callback function can be given
+	 *
+	 * @param string $key
+	 * @param null $callback
+	 * @param string|array|null $tags
+	 * @return mixed|null
+	 */
+	public function getCache(string $key, $callback = null, string|array $tags = null) {
+		$cache = Cache::get($key);
+
+		if ($tags) {
+			$cache = Cache::tags($tags)->get($key);
+		}
+
+		if ($cache) {
+			return $cache;
+		} else {
+			if ($callback) {
+				return $callback();
+			} else {
+				return null;
+			}
+		}
+	}
+
+	/**
+	 * Removes all cached resources
+	 */
+	public function clearAllCache() {
+		Cache::flush();
+	}
+
+	/**
+	 * Removes all cached resources
+	 */
+	public static function clearAllCacheStatically() {
+		(new self)->clearAllCache();
 	}
 
 	/**
@@ -36,27 +83,24 @@ trait CacheTrait
 	 * @param $key
 	 */
 	public static function clearCacheStatically($key) {
-		Cache::forget($key);
+		(new self)->clearCache($key);
 	}
 
 	/**
-	 * Get a keyed Cache value
-	 * If no value found, an optional callback function can be given
+	 * Removes a resource from Cache from given tags
 	 *
-	 * @param $key
-	 * @param null $callback
-	 * @return mixed|null
+	 * @param string|array $tags
 	 */
-	public function getCache($key, $callback = null) {
-		$cache = Cache::get($key);
-		if ($cache) {
-			return $cache;
-		} else {
-			if ($callback) {
-				return $callback();
-			} else {
-				return null;
-			}
-		}
+	public function clearTagCache($tags) {
+		Cache::tags($tags)->flush();
+	}
+
+	/**
+	 * Removes a resource from Cache from given tags
+	 *
+	 * @param string|array $tags
+	 */
+	public static function clearTagCacheStatically($tags) {
+		(new self)->clearTagCache($tags);
 	}
 }
