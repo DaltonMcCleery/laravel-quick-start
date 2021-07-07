1d981d5576f4dbd42640dcfa20a829eff3dbd99f
DaltonMcCleery <daltonmccleery@gmail.com>
2021-07-07 15:41:30 -0500

    PATCH: temp remove full page cache

--- src/Http/Middleware/CachePageResponse.php
+++ src/Http/Middleware/CachePageResponse.php
@@ -14,12 +14,12 @@ class CachePageResponse
 	{
 		$response = $next($request);
 
-		if (
-			$response->headers->has('custom-cached-page')
-			&& (env('APP_ENV') === 'prod' || env('APP_ENV') === 'production')
-		) {
-			$this->setCache($response->headers->get('custom-cached-page'), $response->getContent(), config('quick_start.page_cache_time', '8'));
-		}
+//		if (
+//			$response->headers->has('custom-cached-page')
+//			&& (env('APP_ENV') === 'prod' || env('APP_ENV') === 'production')
+//		) {
+//			$this->setCache($response->headers->get('custom-cached-page'), $response->getContent(), config('quick_start.page_cache_time', '8'));
+//		}
 
 		return $response;
 	}
